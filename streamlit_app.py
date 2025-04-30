bb37b2393b76cbc93d590a8df295c1d7eacb41f9
wcs777courses <wcs777meet@gmail.com>
2025-04-30 16:23:28 +0000

    api integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -20,6 +21,10 @@ chosen_ingredients = st.multiselect(
     options=fruit_df,
     max_selections=5,
 )
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+
 if all([chosen_ingredients, name, st.button('Submit Order')]):
     ingredients = ' '.join(chosen_ingredients)
     statement = f"""
