95344fccbb1f8ac7cbe9f08dbaa73c16c748e86b
wcs777courses <wcs777meet@gmail.com>
2025-04-30 11:11:46 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write("""\
+    Choose the fruits you want in your custom Smoothie!
+""")
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+name = st.text_input('Name on Smoothie')
+st.write(f'The name on your Smoothie will be: {name}')
+
+fruit_df = session.table('smoothies.public.fruit_options').select(col('fruit_name'))
+chosen_ingredients = st.multiselect(
+    label='Choose up to 5 ingredients:',
+    options=fruit_df,
+    max_selections=5,
+)
+if all([chosen_ingredients, name, st.button('Submit Order')]):
+    ingredients = ' '.join(chosen_ingredients)
+    statement = f"""
+        insert into smoothies.public.orders
+            (ingredients, name_on_order) 
+        values 
+            ('{ingredients}', '{name}')
+        """
+    session.sql(statement).collect()
+    st.success('Your Smoothie is ordered!', icon="✅")
